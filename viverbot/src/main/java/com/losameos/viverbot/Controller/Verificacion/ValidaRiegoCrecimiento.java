62a761a0d91d1e97ca4c107ac5ad177e33db5ea6
guillermosalvatierra <guillermosalvatierra@gmail.com>
2017-04-10 08:01:11 -0300

    Se agregan los validadores
    Esto corresponde a la validacion de riego

---
+++ viverbot/src/main/java/com/losameos/viverbot/Controller/Verificacion/ValidaRiegoCrecimiento.java
@@ -0,0 +1,5 @@
+package com.losameos.viverbot.Controller.Verificacion;
+
+public class ValidaRiegoCrecimiento {
+
+}
---
+++ viverbot/src/main/java/com/losameos/viverbot/Controller/Verificacion/ValidaRiegoTemperatura.java
@@ -0,0 +1,5 @@
+package com.losameos.viverbot.Controller.Verificacion;
+
+public class ValidaRiegoTemperatura {
+
+}
