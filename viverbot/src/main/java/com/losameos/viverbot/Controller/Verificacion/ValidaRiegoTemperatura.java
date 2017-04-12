c42e3aa171f26f026e647bb266433676c51c390e
guillermosalvatierra <guillermosalvatierra@gmail.com>
2017-04-11 22:55:57 -0300

    Se agrega Transmision temperatura actual y validaciones
    Es una clase especifica para obtener la temperatura del momento

--- viverbot/src/main/java/com/losameos/viverbot/Controller/Verificacion/ValidaRiegoTemperatura.java
+++ viverbot/src/main/java/com/losameos/viverbot/Controller/Verificacion/ValidaRiegoTemperatura.java
@@ -1,5 +1,18 @@
 package com.losameos.viverbot.Controller.Verificacion;
 
 public class ValidaRiegoTemperatura {
+	TransmisorTemperaturaActual tempActual;
 
+	public ValidaRiegoTemperatura() {
+		tempActual =new TransmisorTemperaturaActual();
+		
+	}
+
+	
+	public boolean temperaturaOptimaParaRiego(){
+		if (tempActual.colector.getInstrumentoMedicion().getMedicion().getValor()> 0){
+			return true;
+		}
+		return false;
+	}
 }
