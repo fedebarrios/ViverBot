80ed8588e40e4475f5071bc9187a65c628fce119
Federico Barrios <fede.barrios@hotmail.com>
2017-05-11 16:04:50 -0300

    Agregamos test
    Se agregaron un par de test mas para tener mas coverage.

---
+++ viverbot/src/test/java/viverbot/Model/Medicion/CalorTest.java
@@ -0,0 +1,28 @@
+package viverbot.Model.Medicion;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import viverbot.Modelo.Medicion.AireAcondicionado;
+import viverbot.Modelo.Medicion.Calor;
+
+public class CalorTest {
+
+	@Test
+	public void anunciarEstadoTest() {
+		AireAcondicionado aire = new AireAcondicionado();
+		Calor calor = new Calor();
+		calor.anunciarEstado(aire);
+		assertEquals(Calor.class, aire.getFrioCalorEstado().getClass());
+	}
+	
+	@Test
+	public void toStringTest() {
+		Calor calor = new Calor();
+		String ret = calor.toString();
+		String expected = "Calor";
+		assertEquals(expected, ret);
+	}
+
+}
---
+++ viverbot/src/test/java/viverbot/Model/Medicion/FrioTest.java
@@ -0,0 +1,27 @@
+package viverbot.Model.Medicion;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import viverbot.Modelo.Medicion.AireAcondicionado;
+import viverbot.Modelo.Medicion.Frio;
+
+public class FrioTest {
+
+	@Test
+	public void anunciarEstadoTest() {
+		AireAcondicionado aire = new AireAcondicionado();
+		Frio frio = new Frio();
+		frio.anunciarEstado(aire);
+		assertEquals(Frio.class, aire.getFrioCalorEstado().getClass());
+	}
+	
+	@Test
+	public void toStringTest() {
+		Frio frio = new Frio();
+		String ret = frio.toString();
+		String expected = "Frio";
+		assertEquals(expected, ret);
+	}
+}
