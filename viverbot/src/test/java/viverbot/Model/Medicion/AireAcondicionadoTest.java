0010f759a38a697f2c3d4d4614720cb38ade6f43
Federico Barrios <fede.barrios@hotmail.com>
2017-05-17 16:35:03 -0300

    arreglaando tests

--- viverbot/src/test/java/viverbot/Model/Medicion/AireAcondicionadoTest.java
+++ viverbot/src/test/java/viverbot/Model/Medicion/AireAcondicionadoTest.java
@@ -3,23 +3,70 @@ package viverbot.Model.Medicion;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
+import java.text.DecimalFormat;
 
+import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import viverbot.Modelo.Magnitudes.Magnitudes;
+import viverbot.Modelo.Magnitudes.Medicion;
 import viverbot.Modelo.Medicion.AireAcondicionado;
 import viverbot.Modelo.Medicion.Calor;
-import viverbot.Modelo.Medicion.Potencia_0;
+import viverbot.Modelo.Medicion.Frio;
+import viverbot.Modelo.Medicion.Potencia_1;
+import viverbot.Modelo.Medicion.TemperaturaAireAcondicionado;
 
 public class AireAcondicionadoTest {
+	private AireAcondicionado aire;
 
 	@Test
 	public void getSetTest() {
-		AireAcondicionado aire = new AireAcondicionado();
-		aire.setEstado(new Calor());
-		aire.setPotencia(new Potencia_0());
+		init();
 		assertNotNull(aire.getEstado());
 		assertNotNull(aire.getPotencia());
-		assertEquals(new Potencia_0().getClass(), aire.getPotencia().getClass());
+		assertNotNull(aire.getTemp());
+		assertEquals(new Potencia_1().getClass(), aire.getPotencia().getClass());
 		assertEquals(new Calor().getClass(), aire.getEstado().getClass());
+		assertEquals(new TemperaturaAireAcondicionado().getClass(), aire.getTemp().getClass());
+	}
+	
+	@Test
+	public void ejecutarTest() throws NumberFormatException{
+		init();
+		Medicion aux = aire.ejecutar();
+		Medicion expected = new Medicion(0.026, Magnitudes.TEMPERATURA);
+		DecimalFormat df = new DecimalFormat("0.000");
+		String s = df.format(aux.getValor());
+		assertNotNull(s);
+		Double ret = Double.parseDouble(s);
+		assertNotNull(ret);
+		assertEquals(expected.getValor(), ret);
+	}
+	
+	@Test
+	public void definirPositivoNegativoTest_calor() throws Exception {
+		init();
+		Double aux = 0.5;
+		Double ret = Whitebox.invokeMethod(aire, "definirPositivoNegativo", 0.5);
+		assertNotNull(ret);
+		assertEquals(aux, ret);
+	}
+	
+	@Test
+	public void definirPositivoNegativoTest_frio() throws Exception {
+		init();
+		aire.setEstado(new Frio());
+		Double aux = -0.5;
+		Double ret = Whitebox.invokeMethod(aire, "definirPositivoNegativo", 0.5);
+		assertNotNull(ret);
+		assertEquals(aux, ret); 
+	}
+	
+	public void init(){
+		this.aire = new AireAcondicionado();
+		aire.setEstado(new Calor());
+		aire.setPotencia(new Potencia_1());
+		aire.setTemperatura("22");
 	}
 
 }
