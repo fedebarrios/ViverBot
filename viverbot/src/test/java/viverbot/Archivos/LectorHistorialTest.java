99677f19c1c14dd46b3e58cfcfe03419ee9c456d
Gaston <gastonsturla01@gmail.com>
2017-05-19 00:30:30 -0300

    Se corrigieron los test de /archivo

--- viverbot/src/test/java/viverbot/Archivos/LectorHistorialTest.java
+++ viverbot/src/test/java/viverbot/Archivos/LectorHistorialTest.java
@@ -1,8 +1,5 @@
 package viverbot.Archivos;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
@@ -11,31 +8,30 @@ import org.junit.Test;
 public class LectorHistorialTest {
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	LectorHistorial lector;
+	LectorTxt lector;
 	
 	@Test (expected = Exception.class)
 	public void testArchivoNoEncontrado() throws Exception
 	{
 		inicialize("src/test/java/viverbot/Archivos/Historial.t");
-		String msjLog = outContent.toString();
-		assertNotNull(msjLog);
-		assertTrue(msjLog.contains("No existe archivo"));
-
 	}
 	
 	@Test (expected = Exception.class)
 	public void testArchivoNoTxt() throws Exception
 	{
 		inicialize("src/test/java/viverbot/Archivos/Historial.csb");
-		String msjLog = outContent.toString();
-		assertNotNull(msjLog);
-		assertTrue(msjLog.contains("La extension del archivo no es .txt."));
+	}
+	
+	@Test
+	public void testLecturaExitosa() throws Exception
+	{
+		inicialize("src/test/java/viverbot/Archivos/Historial.txt");	
 	}
 	
 	private void inicialize(String archivo) throws Exception{
 		System.setOut(new PrintStream(outContent));
 	    System.setErr(new PrintStream(errContent));
-		lector = new LectorHistorial();
-		lector.leerArchivo(archivo);
+		lector = new LectorTxt();
+		lector.leerTxt(archivo);
 	}
 }
--- viverbot/src/test/java/viverbot/Archivos/ParserTest.java
+++ viverbot/src/test/java/viverbot/Archivos/ParserTest.java
@@ -3,6 +3,7 @@ package viverbot.Archivos;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -14,35 +15,32 @@ import viverbot.Modelo.Magnitudes.EmptyMedicion;
 public class ParserTest {
 	
 	@Test
-	public void ParserEspecie(){
+	public void ParserEspecie() throws Exception{
 		ParserEspecie parser = new ParserEspecie();
-		String datos = "etomate:tomatus";
+		String datos = "Especie:(tomate,tomatus)";
 		EspecieDTO especie = parser.parsear(datos);
 		assertEquals("tomate",especie.getNombre());
 		assertEquals("tomatus",especie.getNombreCientifico());
 	}
 	
 	@Test
-	public void ParserTuplaAltura(){
+	public void ParserTuplaAltura() throws Exception{
 		ParserHistorial parser = new ParserHistorial();
-		String datos = "h14:2.05";
-		TuplaAltura tupla = parser.parsear(datos);
-		assertEquals(14,tupla.getDiaDeVida());
-		assertTrue( 2.05 == tupla.getAltura().getValor());
+		String datos = "Dia:(4:6.51cm)";
+		List<TuplaAltura> tuplas = parser.parsear(datos);
+		assertEquals(4,tuplas.get(0).getDiaDeVida());
+		assertEquals( 6.51 , tuplas.get(0).getAltura().getValor() , 0);
 	}
 	
 	@Test
-	public void MediatorTest(){
+	public void MediatorTest() throws Exception{
 		ParserDataArchivos mediator = new ParserDataArchivos();
-		ArrayList<String> datos = new ArrayList<String>();
-		datos.add("h1:1.54");
-		datos.add("h3:1.65");
-		datos.add("ecebolla:cebollus");
+		String datos = "Dia:(4:6.51cm)" + "Dia:(6:8.41cm)" + "Especie:(cebolla,cebollus)" ;
 		HistorialOptimo historial = mediator.parsearHistorialEspecie(datos);
 		assertEquals("cebolla",historial.getEspecie().getNombre());
 		assertEquals("cebollus",historial.getEspecie().getNombreCientifico());
-		assertTrue(1.54 == historial.buscarTupla(1).getAltura().getValor());
-		assertTrue(1.65 == historial.buscarTupla(3).getAltura().getValor());
-		assertTrue(historial.buscarTupla(4).getAltura() instanceof EmptyMedicion);
+		assertEquals(6.51 , historial.buscarTupla(4).getAltura().getValor(), 0);
+		assertEquals(8.41 , historial.buscarTupla(6).getAltura().getValor(), 0);
+		assertTrue(historial.buscarTupla(2).getAltura() instanceof EmptyMedicion);
 	}
 }
