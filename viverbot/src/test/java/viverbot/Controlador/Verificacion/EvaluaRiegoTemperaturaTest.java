099dd518de953d363b978e5b99e315199e020695
guillermosalvatierra <guillermosalvatierra@gmail.com>
2017-05-19 14:29:50 -0300

    Se agrega la evaluacion de humedad y se realizan cambios

---
+++ viverbot/src/test/java/viverbot/Controlador/Verificacion/EvaluaRiegoTemperaturaTest.java
@@ -0,0 +1,63 @@
+package viverbot.Controlador.Verificacion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import viverbot.Controlador.Verificacion.RiegoValidable;
+import viverbot.Controlador.Verificacion.EvaluaRiego;
+import viverbot.Controlador.Verificacion.EvaluaRiegoXTemperatura;
+import viverbot.Modelo.Magnitudes.Magnitudes;
+import viverbot.Modelo.Magnitudes.Medicion;
+
+public class EvaluaRiegoTemperaturaTest {
+
+	private EvaluaRiegoXTemperatura EvaluaRiegoTempTest=null;
+	private EvaluaRiego EvaluaRiegoInicial=null;
+
+	private Medicion temperaturaBaja = new Medicion(5.0,Magnitudes.TEMPERATURA);
+	private Medicion temperaturaAlta = new Medicion(25.0,Magnitudes.TEMPERATURA);
+
+	@Test
+	public void ValidaRiegoTempTest() {
+		this.inicialize();
+		assertTrue(this.EvaluaRiegoTempTest.getTemperaturaActual() != null);
+		this.clear();
+
+	}
+
+	
+	@Test
+	public void AnalizarBajoTest() {
+		this.inicialize();
+		EvaluaRiegoTempTest.setTemperaturaActual(temperaturaBaja);
+				
+		assertEquals(EvaluaRiegoTempTest.cantidadOptimaRiego(),EvaluaRiegoInicial.cantidadOptimaRiego().bajar());
+
+	}
+	
+	@Test
+	public void AnalizarAltoTest() {
+		this.inicialize();
+		EvaluaRiegoTempTest.setTemperaturaActual(temperaturaAlta);
+		
+		assertEquals(EvaluaRiegoTempTest.cantidadOptimaRiego(),EvaluaRiegoInicial.cantidadOptimaRiego().aumentar());
+	}
+	
+	
+
+	// metodos auxiliares
+
+	private void inicialize() {
+		RiegoValidable rv= new EvaluaRiego();
+		EvaluaRiegoInicial=new EvaluaRiego();
+		EvaluaRiegoTempTest=new EvaluaRiegoXTemperatura(rv);
+
+
+		
+	}
+
+	private void clear() {
+		this.EvaluaRiegoTempTest = null;
+	}
+}
