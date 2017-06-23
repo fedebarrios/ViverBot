879d7a1d9655b1ed3db2559015190836297bb9e1
Gaston <gastonsturla01@gmail.com>
2017-06-23 13:47:25 -0300

    Se agrego un test de directorios

--- viverbot/src/test/java/viverbot/Controlador/ControladorConfiguracionTest.java
+++ viverbot/src/test/java/viverbot/Controlador/ControladorConfiguracionTest.java
@@ -16,13 +16,44 @@ public class ControladorConfiguracionTest {
 	ControladorConfiguracionDirectorio conf;
 
 	@Test
-	public void llenarCampos(){
-		inicialize();
+	public void noEligioDirectorio(){
+
+		conf = new ControladorConfiguracionDirectorio();
+		GatewayConfiguracion.limpiar();
+		GatewayConfiguracion.agregarDirectorio("c/usuario");
+		assertEquals(1 , GatewayConfiguracion.getDirectorios().size());	
+
+		GatewayConfiguracion.agregarDirectorio("c/desktop");
+		assertEquals(2 , GatewayConfiguracion.getDirectorios().size());
 		
-		
+		conf.llenarCampos();
+		conf.getVista().getComboBox().setSelectedIndex(0);
+		assertFalse(conf.eligioDirectorio());
+
+		conf.getVista().getComboBox().setSelectedIndex(2);
+		assertTrue(conf.eligioDirectorio());
+	
 	}
 	
-	public void inicialize(){
+
+	@Test
+	public void guardoDirectorio(){
+
 		conf = new ControladorConfiguracionDirectorio();
+		GatewayConfiguracion.limpiar();
+		GatewayConfiguracion.agregarDirectorio("c/usuario");
+		assertEquals(1 , GatewayConfiguracion.getDirectorios().size());	
+
+		GatewayConfiguracion.agregarDirectorio("c/desktop");
+		assertEquals(2 , GatewayConfiguracion.getDirectorios().size());
+		
+		MockFileChooser fileChooser = new MockFileChooser();
+		fileChooser.setFile(new File("C/Documents"));
+		conf.setFileChooser(fileChooser);
+		conf.llenarCampos();
+		conf.actionPerformed(new ActionEvent(conf.getVista().getBtnCargarDirectorio(), 1, ""));
+		
+		assertEquals(3, GatewayConfiguracion.getDirectorios().size());
+	
 	}
 }
