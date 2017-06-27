2ad970ef5ce9d5212cf111698a30527f6d8d3ed2
Javier20105 <xavierescobar3335@gmail.com>
2017-06-27 15:38:54 -0300

    Se modifico un test

--- viverbot/src/test/java/viverbot/Model/Medicion/AnalizadorTemperaturaTest.java
+++ viverbot/src/test/java/viverbot/Model/Medicion/AnalizadorTemperaturaTest.java
@@ -5,50 +5,53 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import viverbot.Model.RangoNumerico;
-import viverbot.Modelo.Magnitudes.EmptyMagnitud;
 import viverbot.Modelo.Magnitudes.Magnitud;
 import viverbot.Modelo.Magnitudes.Medicion;
-import viverbot.Modelo.Magnitudes.Temperatura;
 import viverbot.Modelo.Medicion.AnalizadorTemperatura;
 import viverbot.Modelo.Medicion.DiagnosticoAnalisis;
 import viverbot.Modelo.Medicion.MedicionVacioException;
 
 public class AnalizadorTemperaturaTest {
 	private RangoNumerico rango = new RangoNumerico(10.0, 20.0);
-	private Medicion temperaturaOptima = new Medicion(15.0, Magnitud.TEMPERATURA);
-	private Medicion temperaturaBaja = new Medicion(5.0, Magnitud.TEMPERATURA);
-	private Medicion temperaturaAlta = new Medicion(25.0, Magnitud.TEMPERATURA);
 	private Medicion vacia = new Medicion(0.0, Magnitud.VACIO);
-
-	@Test
-	public void AnalizarOptimoTest() throws MedicionVacioException {
-		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(temperaturaOptima, rango);
-		assertNotNull(resultado);
-		assertEquals(resultado.getOptima(), true);
-		assertEquals(resultado.getValor(), temperaturaOptima);
-		assertTrue(resultado.getDiferencia() == 0.0);
-
-	}
-
+	
 	@Test
 	public void AnalizarBajoTest() throws MedicionVacioException {
-		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(temperaturaBaja, rango);
+		Medicion medicion = new Medicion(8.0, Magnitud.TEMPERATURA);
+
+		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(medicion, rango);
 		assertNotNull(resultado);
 		assertTrue(resultado.getOptima() == false);
-		assertTrue((resultado.getValor()).equals(temperaturaBaja));
-		assertTrue(resultado.getDiferencia() == -5.0);
+		assertTrue((resultado.getValor()).equals(medicion));
+		assertTrue(resultado.getDiferencia() == -2.0);
 
 	}
 
 	@Test
 	public void AnalizarAltoTest() throws MedicionVacioException {
-		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(this.temperaturaAlta, rango);
+		 Medicion medicion = new Medicion(24.0, Magnitud.TEMPERATURA);
+
+		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(medicion, rango);
 		assertTrue(resultado.getOptima() == false);
-		assertTrue(resultado.getValor().equals(this.temperaturaAlta));
+		assertTrue(resultado.getValor().equals(medicion));
 		assertNotNull(resultado);
-		assertTrue(resultado.getDiferencia() == 5.0);
+		assertTrue(resultado.getDiferencia() == 4.0);
 
 	}
+	@Test
+	public void AnalizarOptimoTest() throws MedicionVacioException {
+		Medicion medicion = new Medicion(15.0, Magnitud.TEMPERATURA);
+		DiagnosticoAnalisis resultado = AnalizadorTemperatura.analizar(medicion, rango);
+		assertNotNull(resultado);
+		assertEquals(resultado.getOptima(), true);
+		assertEquals(resultado.getValor(), medicion);
+		assertTrue(resultado.getDiferencia() == 0.0);
+
+	}
+
+	
+
+	
 
 	@Test
 	public void AnalizarVacioTest() {
